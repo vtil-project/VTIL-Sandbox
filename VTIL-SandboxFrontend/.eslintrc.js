436e386e7130676c2c867dad7d04013d562cf64b
Brendan Heinonen <brendan@heinonen.co>
2020-05-06 12:49:03 -0400

    Configured frontend to use vue-cli

---
+++ VTIL-SandboxFrontend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    "browser": true,
+    "commonjs": true,
+    "node": true,
+    "es6": true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
